bab02db654121235881fb62dac1d602185bcc584
Jonas S <jonas1111@gmail.com>
2022-06-13 16:08:17 +0200

    Add test for registry flag

--- source/index.ts
+++ source/index.ts
@@ -1,55 +1,66 @@
 import * as tl from "azure-pipelines-task-lib/task";
 import * as path from "path";
-import { ToolRunner, IExecSyncResult } from "azure-pipelines-task-lib/toolrunner";
+import {
+  ToolRunner,
+  IExecSyncResult,
+} from "azure-pipelines-task-lib/toolrunner";
 
 async function run(): Promise<void> {
-    try {
-        tl.setResourcePath(path.join(__dirname, "task.json"));
-        const cwd: string = tl.getPathInput("path", true, true) || __dirname;
-        const level: string = tl.getInput("level", true) || "high";
-        const productionOnly: boolean = tl.getBoolInput("productionOnly", false);
-        const registry: string = tl.getInput("registry", false) as string;
-        const toolPath: string = tl.which("npm", true);
-        const toolRunner: ToolRunner = tl.tool(toolPath).arg("audit");
+  try {
+    tl.setResourcePath(path.join(__dirname, "task.json"));
+    const cwd: string = tl.getPathInput("path", true, true) || __dirname;
+    const level: string = tl.getInput("level", true) || "high";
+    const toolPath: string = tl.which("npm", true);
+    const toolRunner: ToolRunner = tl.tool(toolPath).arg("audit");
 
-        if (productionOnly) toolRunner.arg("--production");
-        if (registry) toolRunner.arg(`--registry=${registry}`);
+    setProductionOnlyFlag(toolRunner);
+    setRegistry(toolRunner);
 
-        tl.cd(cwd);
-        const result: IExecSyncResult = toolRunner.execSync();
+    tl.cd(cwd);
+    const result: IExecSyncResult = toolRunner.execSync();
 
-        if (result.code !== 0) {
-            const regexp: RegExp = await getLevelRegexp(level);
+    if (result.code !== 0) {
+      const regexp: RegExp = await getLevelRegexp(level);
 
-            const shouldBreak: boolean = regexp.test(result.stdout);
+      const shouldBreak: boolean = regexp.test(result.stdout);
 
-            if (shouldBreak) {
-                console.log(tl.loc("VulnerabilitiesFound"));
-                tl.setResult(tl.TaskResult.Failed, "Vulnerabilities found");
-            } else {
-                console.log(tl.loc("VulnerabilitiesFoundButLowerLevel"));
-            }
-        }
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+      if (shouldBreak) {
+        console.log(tl.loc("VulnerabilitiesFound"));
+        tl.setResult(tl.TaskResult.Failed, "Vulnerabilities found");
+      } else {
+        console.log(tl.loc("VulnerabilitiesFoundButLowerLevel"));
+      }
     }
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err.message);
+  }
+}
+
+function setProductionOnlyFlag(toolRunner: ToolRunner) {
+  const productionOnly: boolean = tl.getBoolInput("productionOnly", false);
+  if (productionOnly) toolRunner.arg("--production");
+}
+
+function setRegistry(toolRunner: ToolRunner) {
+  const registry: string = tl.getInput("registry", false) as string;
+  if (registry) toolRunner.arg(`--registry=${registry}`);
 }
 
 async function getLevelRegexp(level: string): Promise<RegExp> {
-    if (level === "low") {
-        return new RegExp(/\d+ (low|moderate|high|critical)+/gm);
-    }
-    if (level === "moderate") {
-        return new RegExp(/\d+ (moderate|high|critical)+/gm);
-    }
-    if (level === "high") {
-        return new RegExp(/\d+ (high|critical)+/gm);
-    }
-    if (level === "critical") {
-        return new RegExp(/\d+ critical/gm);
-    }
+  if (level === "low") {
+    return new RegExp(/\d+ (low|moderate|high|critical)+/gm);
+  }
+  if (level === "moderate") {
+    return new RegExp(/\d+ (moderate|high|critical)+/gm);
+  }
+  if (level === "high") {
+    return new RegExp(/\d+ (high|critical)+/gm);
+  }
+  if (level === "critical") {
+    return new RegExp(/\d+ critical/gm);
+  }
 
-    throw new Error("Unexpected level");
+  throw new Error("Unexpected level");
 }
 
 run();
