f9ba23f1e5d175c961deb2d0cc17f12c5b2cd0a8
Jonas S <jonas1111@gmail.com>
2023-11-14 14:51:56 +0100

    Update dependencies

--- tests/_suite.ts
+++ tests/_suite.ts
@@ -7,127 +7,139 @@ describe("npm-audit task tests", function() {
 
     after(() => { });
 
-    it("should succeed with required inputs", (done: () => void) => {
+    it("should succeed with required inputs", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "requiredInputs.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, true, "should have succeeded" + tr.stdout);
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(
+                tr.succeeded,
+                true,
+                "should have succeeded" + tr.stdout
+            );
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+            done();
+        });
     });
 
-    it("should fail if missing path", (done: () => void) => {
+    it("should fail if missing path", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "missingPath.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, false, "should have failed");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.strictEqual(
-            tr.errorIssues[0],
-            "Input required: path",
-            "error issue output"
-        );
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, false, "should have failed");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
+            assert.strictEqual(
+                tr.errorIssues[0],
+                "Input required: path",
+                "error issue output"
+            );
+            done();
+        });
     });
 
-    it("should fail if missing level", (done: () => void) => {
+    it("should fail if missing level", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "missingLevel.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, false, "should have failed");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.strictEqual(
-            tr.errorIssues[0],
-            "Input required: level",
-            "error issue output"
-        );
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, false, "should have failed");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
+            assert.strictEqual(
+                tr.errorIssues[0],
+                "Input required: level",
+                "error issue output"
+            );
+            done();
+        });
     });
 
-    it("should fail if invalid level", (done: () => void) => {
+    it("should fail if invalid level", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "invalidLevel.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, false, "should have failed");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.strictEqual(
-            tr.errorIssues[0],
-            "Unexpected level",
-            "error issue output"
-        );
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, false, "should have failed");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
+            assert.strictEqual(
+                tr.errorIssues[0],
+                "Unexpected level",
+                "error issue output"
+            );
+            done();
+        });
     });
 
-    it("should fail if vulnerabilites are found", (done: () => void) => {
+    it("should fail if vulnerabilites are found", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "failOnFoundLevel.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, false, "should have failed");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.strictEqual(
-            tr.errorIssues[0],
-            "Vulnerabilities found",
-            "error issue output"
-        );
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, false, "should have failed");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 1, "should have 1 error issue");
+            assert.strictEqual(
+                tr.errorIssues[0],
+                "Vulnerabilities found",
+                "error issue output"
+            );
+            done();
+        });
     });
 
-    it("should append --production if only production flag is set", (done: () => void) => {
+    it("should append --production if only production flag is set", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "productionFlag.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, true, "should have succeeded");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, true, "should have succeeded");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+            done();
+        });
     });
 
-    it("should append --registry if registry is set", (done: () => void) => {
+    it("should append --registry if registry is set", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "registryPath.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, true, "should have succeeded");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, true, "should have succeeded");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+            done();
+        });
     });
 
-    it("should append --json if jsonOutput is set", (done: () => void) => {
+    it("should append --json if jsonOutput is set", (done: Mocha.Done) => {
         this.timeout(10000);
 
         let tp = path.join(__dirname, "jsonOutput.js");
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
-        tr.run();
-        assert.strictEqual(tr.succeeded, true, "should have succeeded");
-        assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
-        assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
-        done();
+        tr.runAsync().then(() => {
+            assert.strictEqual(tr.succeeded, true, "should have succeeded");
+            assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
+            assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
+            done();
+        });
     });
 });
